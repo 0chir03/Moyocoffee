a9ef3db07d0b81137e05b23113afa47a42b2af79
0chir03 <oshirvandanov@gmail.com>
2025-01-10 15:44:20 +0700

    edit

--- app/Service/AvatarService.php
+++ app/Service/AvatarService.php
@@ -11,19 +11,12 @@ class AvatarService
 {
     public function upload($file)
     {
-        // Создает менеджер изображений
         $manager = new ImageManager(new Driver());
-
-        // Читает загруженный файл
         $image = $manager->read($file);
-
-        // Изменяет размер изображения до 256x256 пикселей
         $image->cover(256, 256);
 
-        // Генерирует уникальное имя файла, используя текущее время
         $filename = time() . '.' . $file->getClientOriginalExtension();
         $path = storage_path('app/public/avatars/' . $filename);
-
         $image->save($path);
 
         $user = Auth::user();
